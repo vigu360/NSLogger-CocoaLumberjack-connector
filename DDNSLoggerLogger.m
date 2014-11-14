74ade54ee8536ce5a6ff5dee3ac465d2068b5327
vigu360 <vignesh@me.com>
2014-11-14 13:47:32 -0400

    Updated DDNSLoggerLogger
    Support for CocoaLumberjack 2.0.0-beta4 changes

--- DDNSLoggerLogger.m
+++ DDNSLoggerLogger.m
@@ -52,16 +52,16 @@ static DDNSLoggerLogger *sharedInstance;
 }
 
 - (void)logMessage:(DDLogMessage *)logMessage {
-    NSString *logMsg = logMessage->logMsg;
-
-    if (formatter) {
+    NSString *logMsg = logMessage.message;
+    
+    if (_logFormatter) {
         // formatting is supported but not encouraged!
-        logMsg = [formatter formatLogMessage:logMessage];
+        logMsg = [_logFormatter formatLogMessage:logMessage];
     }
-
+    
     if (logMsg) {
         int nsloggerLogLevel;
-        switch (logMessage->logFlag) {
+        switch (logMessage.flag) {
                 // NSLogger log levels start a 0, the bigger the number,
                 // the more specific / detailed the trace is meant to be
             case LOG_FLAG_ERROR: nsloggerLogLevel = 0; break;
@@ -69,8 +69,8 @@ static DDNSLoggerLogger *sharedInstance;
             case LOG_FLAG_INFO: nsloggerLogLevel  = 2; break;
             default: nsloggerLogLevel             = 3; break;
         }
-
-        LogMessageF(logMessage->file, logMessage->lineNumber, logMessage->function, [logMessage fileName],
+        
+        LogMessageF([logMessage.file UTF8String], (int)logMessage.line, [logMessage.function UTF8String], [logMessage fileName],
                     nsloggerLogLevel, @"%@", logMsg);
     }
 }
